c91d21bfe08f09bdbe60d514c5df93e47b74b9e4
gdesrosi00 <gdesrosi@calpoly.edu>
2019-04-08 10:59:09 -0700

    added functions for reverse_rec and bin_search

--- lab1.py
+++ lab1.py
@@ -1,15 +1,32 @@
 
 def max_list_iter(int_list):  # must use iteration not recursion
-   """finds the max of a list of numbers and returns the value (not the index)
-   If int_list is empty, returns None. If list is None, raises ValueError"""
-   pass
+    if int_list == None:
+        raise ValueError
+    elif len(int_list) == 0:
+        return None
+    x = int_list[0]
+    for i in int_list:
+        if i > x:
+            x = i
+    return x
 
 def reverse_rec(int_list):   # must use recursion
-   """recursively reverses a list of numbers and returns the reversed list
-   If list is None, raises ValueError"""
-   pass
+    if int_list == None:
+        raise ValueError
+    if len(int_list) == 0:
+        return []
+    return reverse_rec(int_list[1:len(int_list)]) + int_list[0:1]
+    #"""recursively reverses a list of numbers and returns the reversed list
+    #If list is None, raises ValueError"""
 
 def bin_search(target, low, high, int_list):  # must use recursion
-   """searches for target in int_list[low..high] and returns index if found
-   If target is not found returns None. If list is None, raises ValueError """
-   pass
+    mid = (low + high) // 2
+    if int_list[mid] == target:
+        return mid
+    if int_list[mid] > target:
+        high = mid - 1
+    elif int_list[mid] < target:
+        low = mid + 1
+    return bin_search(target, low, high, int_list)
+    #"""searches for target in int_list[low..high] and returns index if found
+    #If target is not found returns None. If list is None, raises ValueError """
